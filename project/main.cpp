6dca485c0cb7403a8ad0e1fa0ba2e08bcc680968
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-10-26 13:56:08 +0200

    neatly formatted code

--- project/main.cpp
+++ project/main.cpp
@@ -19,13 +19,16 @@ using namespace std;
 const int64_t SIZE = 16384ULL;
 const int64_t REPEAT = 1ULL;
 
-int main(int argc, char* argv[]) {
-	float* res = new float[SIZE * SIZE];  // result of mean filter
-	float* img = new float[SIZE * SIZE];
+int main(int argc, char *argv[])
+{
+	float *res = new float[SIZE * SIZE]; // result of mean filter
+	float *img = new float[SIZE * SIZE];
 
 	// fill with dummy data
-	for (int64_t i = 0; i < SIZE; i++) {
-		for (int64_t j = 0; j < SIZE; j++) {
+	for (int64_t i = 0; i < SIZE; i++)
+	{
+		for (int64_t j = 0; j < SIZE; j++)
+		{
 			img[j * SIZE + i] = (2 * j + i) % 32768;
 		}
 	}
@@ -34,16 +37,21 @@ int main(int argc, char* argv[]) {
 	uint64_t dummy = 0;
 
 	// ADJUST BELOW, BUT keep writing to the dummy variable
-	
+
 	// Apply an averaging imaging filter to some input image, and write in to an output image.
 	// A pixel in the output image is calculated by averaging 9 pixels: the pixel at the same
 	// coordinates in the input image, and the adjecent pixels.
-	for (int64_t r = 0; r < REPEAT; ++r) {
-		for (int64_t i = 1; i < SIZE - 1; i++) {
-			for (int64_t j = 1; j < SIZE - 1; j++) {
+	for (int64_t r = 0; r < REPEAT; ++r)
+	{
+		for (int64_t i = 1; i < SIZE - 1; i++)
+		{
+			for (int64_t j = 1; j < SIZE - 1; j++)
+			{
 				res[j * SIZE + i] = 0;
-				for (long k = -1; k < 2; k++) {
-					for (long l = -1; l < 2; l++) {
+				for (long k = -1; k < 2; k++)
+				{
+					for (long l = -1; l < 2; l++)
+					{
 						res[j * SIZE + i] += img[(j + l) * SIZE + i + k];
 					}
 				}
@@ -51,8 +59,10 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
-		for (int64_t i = 1; i < SIZE - 1; i++) {
-			for (int64_t j = 1; j < SIZE - 1; j++) {
+		for (int64_t i = 1; i < SIZE - 1; i++)
+		{
+			for (int64_t j = 1; j < SIZE - 1; j++)
+			{
 				dummy += res[j * SIZE + i];
 			}
 		}
@@ -70,7 +80,7 @@ int main(int argc, char* argv[]) {
 	std::cout << "soft page faults:             " << usage.ru_minflt << std::endl;
 	std::cout << "hard page faults:             " << usage.ru_majflt << std::endl;
 #ifdef __APPLE__
-	std::cout << "max memory:                   " << usage.ru_maxrss/1024 << " KiB" << std::endl;
+	std::cout << "max memory:                   " << usage.ru_maxrss / 1024 << " KiB" << std::endl;
 #else
 	std::cout << "max memory:                   " << usage.ru_maxrss << " KiB" << std::endl;
 #endif
